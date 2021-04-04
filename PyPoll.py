263ea075d7e8b1c2035058a98fed9d50b2793cf2
dhanaprakash1 <44513124+dhanaprakash1@users.noreply.github.com>
2021-04-03 20:53:05 -0700

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -5,7 +5,7 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 
-# Import the datetime class from the datatime module 
+# Add Dependencies
 import datetime as dt
 import csv
 import os
@@ -23,8 +23,23 @@ print(file_to_load)
 file_to_save = os.path.join("analysis", "election_analysis.txt") 
 print(file_to_save)
 
+# Initialize a total vote counter
+total_votes = 0
+
+# Candidate Options and candidate votes
+candidate_options = []
+# Declare teh empty dictionary
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker 
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file
 # election_data = open(file_to_load, 'r')
+# Close the file
+# election_data.close()
 with open(file_to_load) as election_data:
     # To do: perform analysis
     print(election_data)
@@ -37,11 +52,60 @@ with open(file_to_load) as election_data:
 
     #Print each row in teh CSV file
     for row in file_reader:
-        print(row)
+        # Add to the total vote count
+        total_votes += 1
+        # print(row)
 
-# Close the file
-# election_data.close()
+        # Print the candidate name from each row
+        candidate_name = row[2]
 
+        # Add the candidate name to the candidate list
+        # If teh candidate does not match any existing candidate...candidate_options
+        if candidate_name not in candidate_options:
+            # Add itt to the list of candidates
+            candidate_options.append(candidate_name)
+            # Begin tracking candidate vote count
+            candidate_votes[candidate_name] = 0
+        
+        # Add a vote to the candidate_count
+        candidate_votes[candidate_name] += 1
+
+# Print teh candidate vote dictionary
+print(candidate_votes)
+
+# Print then candiadte list 
+print(candidate_options)
+
+# 3. Print the total votes
+print(total_votes)
+
+# Determine the percentage of votes for each candidate by looping through the count 
+# Iterate through the candidate list
+for candidate_name in candidate_votes:
+    # Retrive vote count of a candidate 
+    votes = candidate_votes[candidate_name]
+    # Calculate the percentage of votes 
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    #To do: print out each candidate's name, vote count and the percentage of votes to teh termoinal 
+    print(f"{candidate_name}: {vote_percentage:1f}% ({votes:,})\n")
+
+    # Determine winning count and candidate
+    # Determine if the votes is greater than the winning count 
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent = vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # And set the winning_candidate equal to the candidate's name
+        winning_candidate = candidate_name 
+
+# To do: print out the winning candidate, vote count and percentage to the terminal
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n")
+print(winning_candidate_summary)
 
 # Using the open() function with the "w" mode we will write data to teh file
 # outfile = open(file_to_save, "w")
